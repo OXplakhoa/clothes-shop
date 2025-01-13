f5dc3456a0b55c615a6ac910902927345a6d424c
OXplakhoa <OXplakhoa@gmail.com>
2025-01-13 10:55:31 +0700

    Consuming Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Header.jsx';
-import Shop from './components/Shop.jsx';
-import { DUMMY_PRODUCTS } from './dummy-products.js';
-import { CartContext } from './store/shopping-cart-context.jsx';
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import { CartContext } from "./store/shopping-cart-context.jsx";
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
     items: [],
@@ -67,7 +67,7 @@ function App() {
 
   return (
     //If react version < 19 use <CartContext.Provider> and for the closing tag too
-    <CartContext> 
+    <CartContext value={{ items: [] }}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
